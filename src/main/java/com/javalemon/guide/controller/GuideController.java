e55c774936b4a397257673deb695424cb075a650
limeng <seemoonup@gmail.com>
2019-07-14 00:35:18 +0800

    guide

---
+++ src/main/java/com/javalemon/guide/controller/GuideController.java
@@ -0,0 +1,31 @@
+package com.javalemon.guide.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author lemon
+ * @date 2019-07-13
+ * @desc
+ */
+
+@Controller
+public class GuideController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/guide")
+    public String guide(Model model) {
+        model.addAttribute("userInfo", "lemon");
+        return "guide";
+    }
+
+}
---
+++ src/test/java/com/javalemon/guide/GuideApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javalemon.guide;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuideApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
